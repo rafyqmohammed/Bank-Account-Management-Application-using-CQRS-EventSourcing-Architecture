b27e96f6314008cae1daebc5b92d44bb17f28119
rafyqmohammed <rafyq.mohammed.111@gmail.com>
2025-09-19 11:32:00 +0200

    second commit for configeration projet

---
+++ App-Bank-EvenSou-Cqrs/src/main/java/com/example/demo/appbankevensoucqrs/AppBankEvenSouCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.appbankevensoucqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBankEvenSouCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBankEvenSouCqrsApplication.class, args);
+    }
+
+}
---
+++ App-Bank-EvenSou-Cqrs/src/test/java/com/example/demo/appbankevensoucqrs/AppBankEvenSouCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.appbankevensoucqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankEvenSouCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
